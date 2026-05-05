d7edf1327e3de4a655c7fbaca7db8a8e2ea39311
Satyam <satyam3691216@gmail.com>
2026-05-05 13:54:01 +0530

    Cleaned dataset by handling missing values and converting TotalCharges to numeric format

--- Project.py
+++ Project.py
@@ -8,3 +8,22 @@ print("Dataset loaded successfully\n")
 print(df.head())
 print("\nShape:", df.shape)
 print("\nColumns:\n", df.columns)
+print("\n--- Step 2: Data Cleaning ---")
+
+# 1. Drop ID column
+if 'customerID' in df.columns:
+    df.drop(columns=['customerID'], inplace=True)
+
+# 2. Fix TotalCharges (very important for this dataset)
+if 'TotalCharges' in df.columns:
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+
+# 3. Check missing values
+print("\nMissing values BEFORE cleaning:\n", df.isnull().sum())
+
+# 4. Remove missing rows
+df.dropna(inplace=True)
+
+# 5. Verify
+print("\nMissing values AFTER cleaning:\n", df.isnull().sum())
+print("\nCleaned Shape:", df.shape)
